c1f9eec0bccc9040570395d47193f45bcea5a5a8
Danne1987 <164018827+Danne1987@users.noreply.github.com>
2024-05-15 18:28:38 +0200

    tested the writer

--- src/Controller/ScoreController.java
+++ src/Controller/ScoreController.java
@@ -4,8 +4,9 @@ import java.io.*;
 public class ScoreController {
 
     public void writeToFile(){
-        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("files/Highscore.txt")))){
+        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("files/Highscore.txt", true)))){
             writer.write("Testing!!!");
+            writer.newLine();
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
