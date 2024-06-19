e87fd5fbba398919a4414824381d6d33e33d820e
Sarah <sarahkjaerulffnehrkorn@gmail.com>
2024-06-19 10:27:42 +0200

    created interface for hiddenobjects implemented interface on Trap

---
+++ src/Model/HiddenObject.java
@@ -0,0 +1,6 @@
+package Model;
+
+public interface HiddenObject {
+    String[][] placeOnMap(String[][] map);
+    void markDug(String[][] map, int row, int col);
+}
--- src/Model/Trap.java
+++ src/Model/Trap.java
@@ -3,25 +3,38 @@ import java.util.Objects;
 import java.util.Random;
 
 
-public class Trap {
+public class Trap implements HiddenObject {
     static Random random = new Random();
     int x;
     int y;
-    public String[][] PlaceTraps(String[][] map)
-    {
+
+    /**
+     * @param map
+     * @return
+     */
+    @Override
+    public String[][] placeOnMap(String[][] map) {
         boolean validTrapPlace = true;
 
-        while(validTrapPlace)
-        {
+        while(validTrapPlace) {
             x = random.nextInt(map.length);
             y = random.nextInt(map[0].length);
 
-            if(!Objects.equals(map[x][y], "T"))
-            {
+            if(!Objects.equals(map[x][y], "T")) {
                 map[x][y] = "D";
                 validTrapPlace = false;
             }
         }
         return map;
+    }
+
+    /**
+     * @param map
+     * @param row
+     * @param col
+     */
+    @Override
+    public void markDug(String[][] map, int row, int col) {
+        map[row][col] = "DUG";
     }
 }
