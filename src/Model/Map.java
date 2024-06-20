2da01d28d87746d9734543a2650b32e77ca3dcdc
Sarah <sarahkjaerulffnehrkorn@gmail.com>
2024-06-20 22:28:06 +0200

    changed spelPlan to be map

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -1,31 +1,31 @@
 package Model;
 
 public class Map {
-    private String[][] spelPlan;
+    private String[][] map;
     private Treasure[] treasures;
     private Trap[] traps;
 
     public Map() {
-        spelPlan = new String[10][10];
-        for(int i = 0; i < spelPlan.length; i++) {
-            for(int j = 0; j < spelPlan[i].length; j++) {
-                spelPlan[i][j] = "";
+        map = new String[10][10];
+        for(int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map[i].length; j++) {
+                map[i][j] = "";
             }
         }
         treasures = new Treasure[5];
         for (int i = 0; i < treasures.length; i++) {
             treasures[i] = new Treasure(i); // Assuming x = 0 for all objects
-            spelPlan = treasures[i].placeOnMap(spelPlan);
+            map = treasures[i].placeOnMap(map);
         }
         traps = new Trap[3];
         for(int i = 0; i < traps.length; i++) {
             traps[i] = new Trap();
-            traps[i].placeOnMap(spelPlan);
+            traps[i].placeOnMap(map);
         }
     }
 
     public String[][] getMap() {
-        return spelPlan;
+        return map;
     }
 
     public Treasure[] getTreasures() {
